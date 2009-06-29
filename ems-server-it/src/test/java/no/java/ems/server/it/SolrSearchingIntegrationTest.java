c2be57f29d31d2e07ebc73744f5347ded19b1364
Erlend Hamnaberg <ngarthl@gmail.com>
2009-06-29 23:34:06 +0200

    - Added licence and copyright information

--- ems-server-it/src/test/java/no/java/ems/server/it/SolrSearchingIntegrationTest.java
+++ ems-server-it/src/test/java/no/java/ems/server/it/SolrSearchingIntegrationTest.java
@@ -1,12 +1,16 @@
-/* 
- * $Header: $ 
- * 
- * Copyright (C) 2008 Escenic. 
- * All Rights Reserved.  No use, copying or distribution of this 
- * work may be made except in accordance with a valid license 
- * agreement from Escenic.  This notice must be 
- * included on all copies, modifications and derivatives of this 
- * work. 
+/*
+ * Copyright 2009 JavaBin
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package no.java.ems.server.it;
 
